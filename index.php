4e93ce26b5da6a8d2079f40b4ff5e85a6bcc8087
Juan Pablo Velandia Fraija <juanpez5@gmail.com>
2013-01-16 20:21:35 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+
+Datos de Conexión Base de Datos
+
+- usuario: user_test
+- contraseña: password_test
+- base de datos: db_test
+- host: 127.0.0.1
+
+id , fecha (ej 17 de enero, 2012), monto, dias, estado
+
+	- Pagos
+
+	fecha (ej 17 de enero, 2012), total pagado
+
+		- Si no hay pagos o si el monto es de cero mostrar un mensaje diciendo que no hay pagos para esa solicitud
+
+Opcional:
+
+	- No mostrar los pagos al cargar la pagina
+	- Usando javascript debe ser posible elegir si quiero o no mostrar los pagos
+
+
+Enlaces que pueden ser utiles:
+
+	- http://code.jquery.com/jquery.min.js
+	- http://twitter.github.com/bootstrap/assets/css/bootstrap.css
+ */ 
+
